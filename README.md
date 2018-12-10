968d469aac5a86284b345a7751d9609198e64fab
Kheeran <kn16063@my.bristol.ac.uk>
2018-12-10 05:00:43 +0000

    Final Cleanupgit add .

--- README.md
+++ README.md
@@ -10,3 +10,5 @@
 #### The main code is in Q3.py which runs iteratively on all the images, re-producing the results. This takes upto 50 minutes. There are 2 Jupyter Notebooks, 1 is a demo which runs the code on 1 iteration, showing the intemediary steps, the other was used to produce some graphs for the report
 ------------------------------------------------------------------------
 ### Subtask 4: Improving your Detector
+#### we have edited the library slightly for this stage and so made a new one called libraryQ4.py which Q4.py loads from. Q4 runs iteratively through all images, it's very similar to Q3, just with different parameters.
+------------------------------------------------------------------------
---
+++ Subtask2/Q2.py
@@ -0,0 +1,48 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import math
+import matplotlib.pylab as pylab
+
+pylab.rcParams['figure.figsize'] = (20,10)
+
+
+def imshow(image):
+    #OpenCV stores images in BGR so we have to convert to RGB to display it using matplotlib
+    imagergb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    plt.imshow(imagergb)
+
+obj_classifier = cv2.CascadeClassifier('classifier/dartcascade/cascade.xml')
+
+fig = plt.figure()
+#number of test images
+N=16
+
+for i in range (0,N):
+    #load image
+    location = str("../images/dart") + str(i) + str(".jpg")
+    image = cv2.imread(location)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    #run classifier
+    obj = obj_classifier.detectMultiScale(gray, 1.1, 1, 0|cv2.CASCADE_SCALE_IMAGE, (50,50), (500,500))
+
+    #check if emppty
+    if obj is ():
+        print('No objects found in ' + str("dart") + str(i) + str(".jpg"))
+
+    # Visualise classifier: Draw box by iteration
+    for (x,y,w,h) in obj:
+        cv2.rectangle(image, (x,y), (x+w,y+h), (0,255,0), 3)
+
+    saveloc = (str("dartclassified/dartclass" + str(i) + str(".jpg")))
+    cv2.imwrite(saveloc,image)
+#     plot figures
+    cols = 4
+    rows = math.ceil(N/cols)
+    ax = fig.add_subplot(rows, cols, i+1)
+    imagergb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    ax.imshow(imagergb)
+# imshow(image)
+#cv2.imwrite('detected.jpg', detected)
+#cv2.destroyAllWindows()
